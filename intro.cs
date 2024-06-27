4233ef245343cbc1301179615796fb250120e390
Talha Ahmad <talha.code92@gmail.com>
2024-06-27 15:30:08 +0500

    Add files via upload
    Introduction to C# - Source Code | Dated: 27th June, 2024

---
+++ intro.cs
@@ -0,0 +1,21 @@
+﻿/*
+ C#, a popular general-purpose language, shines in software development for its versatility and strong foundation.
+Widely used with the .NET framework ([https://learn.microsoft.com/en-us/dotnet/csharp/](https://learn.microsoft.com/en-us/dotnet/csharp/)), it creates a variety of applications, from web and mobile to enterprise and games.
+Its object-oriented nature promotes code reusability and maintainability, while its large community and clear syntax make learning and development smoother.
+ */
+
+// Namespaces in C# are hierarchical groupings that organize code elements like classes and functions.
+namespace Introduction
+{
+    // "internal class Program{}" defines a class named Program with limited accessibility within the same assembly.
+    internal class Program
+    {
+
+        // This line defines the entry point of a C# program, where execution begins.
+        static void Main(string[] args)
+        {
+            // This line of C# code prints the message "Hello, World!" to the console window.
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
