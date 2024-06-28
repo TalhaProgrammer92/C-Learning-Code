80212f62bfbdfebd93062efbc90144c9ee0b6ec4
Talha Ahmad <talha.code92@gmail.com>
2024-06-28 11:46:47 +0500

    Add files via upload
    Source code of float datatypes in C# | Dated: 28-06-2024

---
+++ FloatDatatypes.cs
@@ -0,0 +1,28 @@
+﻿namespace Float
+{
+    internal class FloatDatatypes
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Datatype ----- Size ----- .NET type ----- Precision (number of total digits)
+
+            float ----- 32 bit ----- System.Single ----- 7
+
+            double ----- 64 bit ----- System.Double ----- 15, 16
+
+            decimal ----- 128 bit ----- System.Decimal ----- 28, 29
+            */
+            
+            // Varaibles
+            float variable_float = 123.31113f;                  // 'f' is compulsory
+            double variable_double = 123.321654987d;             // 'd' is optional
+            decimal variable_decimal = 45567.4235532534436m;    // 'm' is compulsory
+            
+            // Output
+            Console.WriteLine("Float:\t\t{0}", variable_float);
+            Console.WriteLine("Double:\t\t{0}", variable_double);
+            Console.WriteLine("Decimal:\t{0}", variable_decimal);
+        }
+    }
+}
